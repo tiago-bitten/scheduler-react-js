56d28cde774326ad4e80cdac1a3fee859fc17c59
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-02-04 15:25:40 -0300

    impr: LoginRightCard

--- src/components/LoginRightCard.jsx
+++ src/components/LoginRightCard.jsx
@@ -1,69 +1,91 @@
 import React from 'react';
-
-import instance from '../config/axiosConfig';
-
+import { useFormik } from 'formik';
+import * as yup from 'yup';
 import { useNavigate } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
-
-import DefaultInput from '../components/DefaultInput';
+import { TextField } from '@mui/material';
 import RoundButton from '../components/RoundButton';
 import ForgotPassword from './ForgotPassword';
 import CreateAccountLink from './CreateAccountLink';
 import OrBar from './OrBar';
 
+import instance from '../config/axiosConfig';
+
+const loginSchema = yup.object({
+    email: yup.string().email('Email inválido').required('O email é obrigatório'),
+    password: yup.string().required('A senha é obrigatória'),
+});
 
 const LoginRightCard = () => {
-    const [email, setEmail] = React.useState('');
-    const [password, setPassword] = React.useState('');
-
     const { enqueueSnackbar } = useSnackbar();
     const navigate = useNavigate();
 
-    const handleLogin = async () => {
-        try {
-            const response = await instance.post('/auth/signin', {
-                email,
-                password
-            });
+    const formik = useFormik({
+        initialValues: {
+            email: '',
+            password: '',
+        },
+        validationSchema: loginSchema,
+        onSubmit: async (values) => {
+            try {
+                const response = await instance.post('/auth/signin', {
+                    email: values.email,
+                    password: values.password
+                });
 
-            const data = response.data;
+                const data = response.data;
 
-            if (response.status === 200) {
-                sessionStorage.setItem('token', data.token);
-                sessionStorage.setItem('user', JSON.stringify(data.user));
-                enqueueSnackbar('Login efetuado com sucesso.', { variant: 'success' });
-                navigate('/voluntarios');
+                if (response.status === 200) {
+                    sessionStorage.setItem('token', data.token);
+                    sessionStorage.setItem('user', JSON.stringify(data.user));
+                    enqueueSnackbar('Login efetuado com sucesso.', { variant: 'success' });
+                    navigate('/voluntarios');
+                }
+            } catch (error) {
+                if (error.response.status === 401) {
+                    enqueueSnackbar(error.response.data.message, { variant: 'error' });
+                }
             }
-
-        } catch (error) {
-            if (error.response.status === 401) {
-                enqueueSnackbar(error.response.data.message, { variant: 'error' });
-            }
-        }
-    };
-
-    const handleEmailChange = (event) => {
-        setEmail(event.target.value);
-    };
-
-    const handlePasswordChange = (event) => {
-        setPassword(event.target.value);
-    };
+        },
+    });
 
     return (
         <div className="flex flex-col justify-center items-center w-full">
             <h1 className="text-4xl text-center mb-10 text-quinary">Bem-vindo</h1>
-            <div className="mb-10">
-                <DefaultInput label="Email" id="email" type="text" onChange={handleEmailChange} />
-            </div>
-            <div className="mb-10 flex flex-col items-end">
-                <DefaultInput label="Senha" id="password" type="password" onChange={handlePasswordChange} />
-                <div className="mt-2">
-                    <ForgotPassword />
-                </div>
-            </div>
-            <div className="mb-8">
-                <RoundButton value="ENTRAR" onClick={() => handleLogin()} />
+            <div className="flex flex-col items-center justify-center w-full">
+                <form onSubmit={formik.handleSubmit} style={{ width: '60%' }}>
+                    <TextField
+                        label="Email"
+                        id="email"
+                        type="email"
+                        variant="standard"
+                        required
+                        fullWidth
+                        autoComplete="off"
+                        sx={{ mb: 4 }}
+                        {...formik.getFieldProps('email')}
+                        error={formik.touched.email && Boolean(formik.errors.email)}
+                        helperText={formik.touched.email && formik.errors.email}
+                    />
+                    <TextField
+                        label="Senha"
+                        id="password"
+                        type="password"
+                        variant="standard"
+                        required
+                        fullWidth
+                        autoComplete="off"
+                        {...formik.getFieldProps('password')}
+                        error={formik.touched.password && Boolean(formik.errors.password)}
+                        helperText={formik.touched.password && formik.errors.password}
+                    />
+                    <div className="mt-2 mb-10 text-right">
+                        <ForgotPassword />
+                    </div>
+                    <div className="flex justify-center w-full mb-8">
+                        <RoundButton value="ENTRAR" type="submit" />
+                    </div>
+                </form>
             </div>
             <div className="w-1/2">
                 <OrBar />
--- src/components/RoundButton.jsx
+++ src/components/RoundButton.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 
-const RoundButton = ({ value, onClick }) => {
+const RoundButton = ({ value, type, onClick }) => {
     return (
         <button
             className="bg-secondary text-white font-regular py-2 px-9 rounded-full hover:bg-primary transition duration-500 ease-out transform hover:scale-105 active:scale-95"
-            type="button"
+            type={type || 'button'}
             onClick={onClick}
         >
             {value}
