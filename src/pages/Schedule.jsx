3db2998865fc48c8359f01944d731ccbca094816
BuildTools <tiago.sbittencourt@icloud.com>
2024-02-10 17:58:18 -0300

    impr: schedules

--- src/pages/Schedule.jsx
+++ src/pages/Schedule.jsx
@@ -3,15 +3,15 @@ import { useNavigate } from 'react-router-dom';
 import { useSnackbar } from 'notistack';
 import MyCalendar from '../components/MyCalender';
 import moment from 'moment';
+import useFetch from '../hooks/useFetch';
 
-import instance from '../config/axiosConfig';
 import Header from '../components/Header';
 import OpenScheduleModal from '../components/OpenScheduleModal';
 import AppointmentModal from '../components/AppointmentModal';
 import RoundButton from '../components/RoundButton';
 
 const Schedule = () => {
-    const [token] = useState(sessionStorage.getItem('token'));
+    const { data, fetch } = useFetch(`/schedules?month=${month}&year=${year}`);
     const [schedules, setSchedules] = useState([]);
     const [month, setMonth] = useState(new Date().getMonth() + 1);
     const [year, setYear] = useState(new Date().getFullYear());
@@ -20,18 +20,25 @@ const Schedule = () => {
     const [selectedDate, setSelectedDate] = useState(null);
     const [selectedEvent, setSelectedEvent] = useState(null);
 
-    const { enqueueSnackbar } = useSnackbar();
     const navigate = useNavigate();
 
-    useEffect(() => {
-        if (!token) {
-            navigate('/login');
-            enqueueSnackbar('Você precisa estar logado para acessar essa página.', { variant: 'error' });
-            return;
-        }
+    React.useEffect(() => {
+        fetch();
+    }, [fetch, month, year, navigate]);
 
-        fetchSchedules();
-    }, [token, month, year, navigate]);
+    React.useEffect(() => {
+        if (data) {
+            const schedulesData = data.schedules.map(schedule => ({
+                title: schedule.name,
+                start: new Date(schedule.startDate),
+                end: new Date(schedule.endDate),
+                allDay: false,
+                id: schedule.id,
+                description: schedule.description
+            }));
+            setSchedules(schedulesData);
+        }
+    }, [data]);
 
     const handleClickOpenSchedule = () => {
         setOpenScheduleModal(true);
@@ -53,35 +60,6 @@ const Schedule = () => {
         setYear(date.getFullYear());
     };
 
-    const fetchSchedules = async () => {
-        try {
-            const response = await instance.get(`/schedules?month=${month}&year=${year}`, {
-                headers: {
-                    Authorization: `Bearer ${token}`,
-                },
-            });
-
-            if (response.status === 200) {
-                console.log(response.data);
-                const schedulesData = response.data.schedules.map(schedule => ({
-                    title: schedule.name,
-                    start: new Date(schedule.startDate),
-                    end: new Date(schedule.endDate),
-                    allDay: false,
-                    id: schedule.id,
-                    description: schedule.description
-                }));
-                setSchedules(schedulesData);
-            }
-
-        } catch (err) {
-            if (err.response.status === 401) {
-                navigate('/entrar');
-                enqueueSnackbar('Sessão expirada, faça login novamente.', { variant: 'error' });
-            }
-        }
-    }
-
     return (
         <>
             <Header />
@@ -100,7 +78,7 @@ const Schedule = () => {
                 open={showOpenScheduleModal}
                 onClose={() => setOpenScheduleModal(false)}
                 selectedDate={selectedDate}
-                fetchSchedules={fetchSchedules}
+                fetchSchedules={fetch}
             />
             <AppointmentModal
                 open={showEventModal}
