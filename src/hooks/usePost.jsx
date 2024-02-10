55807e21271562e70485ff17af6d4ba7bf7bf000
BuildTools <tiago.sbittencourt@icloud.com>
2024-02-09 23:24:01 -0300

    usePost

---
+++ src/hooks/usePost.jsx
@@ -0,0 +1,23 @@
+import { useState } from 'react';
+import instance from '../config/axiosConfig';
+
+const usePost = (url) => {
+    const [data, setData] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    const post = (body) => {
+        const headers = {
+            'Content-Type': 'application/json',
+            'Authorization': `Bearer ${sessionStorage.getItem('token')}`
+        };
+
+        setLoading(true);
+        instance.post(url, body, { headers })
+            .then((response) => setData(response.data))
+            .catch((error) => setError(error))
+            .finally(() => setLoading(false));
+    };
+
+    return { data, error, loading, post };
+};
