5ff89a0dfb219fb806e476fb0b2ba4d54b27c37e
BuildTools <tiago.sbittencourt@icloud.com>
2024-02-18 14:02:13 -0300

    impr: volunteers

--- src/components/VolunteerListSkeleton.jsx
+++ src/components/VolunteerListSkeleton.jsx
@@ -1,9 +1,5 @@
 import React from 'react';
 import { Skeleton } from '@mui/material';
-import DefaultInput from './DefaultInput';
-import RoundButton from './RoundButton';
-import Switch from '@mui/material/Switch';
-import Header from './Header';
 
 const VolunteerSkeleton = () => {
     return (
@@ -25,32 +21,11 @@ const VolunteerSkeleton = () => {
 
 const VolunteerListSkeleton = () => {
     return (
-        <>
-            <Header />
-            <div className="flex justify-between items-center mt-16 mx-12">
-                <div className="flex flex-1 gap-4">
-                    <DefaultInput label="Voluntários" id="voluntarios" />
-                    <DefaultInput label="Ministérios" id="ministerios" />
-                </div>
-                <div>
-                    <RoundButton value="CADASTRAR VOLUNTÁRIO" onClick={null} />
-                </div>
-            </div>
-            <div className="ml-10 mt-4">
-                <Switch
-                    checked={null}
-                    onChange={null}
-                    name="checked"
-                    color="primary"
-                />
-                <span className="text-quinary">Apenas voluntários com ministérios</span>
-            </div>
-            <div className="bg-septenary p-4 mx-12 mt-12 animate-pulse">
-                {Array.from(new Array(10)).map((_, index) => (
-                    <VolunteerSkeleton key={index} />
-                ))}
-            </div>
-        </>
+        <div className="bg-septenary p-4 mx-12 mt-12 animate-pulse">
+            {Array.from(new Array(10)).map((_, index) => (
+                <VolunteerSkeleton key={index} />
+            ))}
+        </div>
     );
 };
 
--- src/pages/Volunteer.jsx
+++ src/pages/Volunteer.jsx
@@ -1,171 +1,188 @@
-import React from 'react';
-import { useNavigate } from 'react-router-dom';
-import Header from '../components/Header';
-import DefaultInput from '../components/DefaultInput';
-import RoundButton from '../components/RoundButton';
-import Switch from '@mui/material/Switch';
-import NotFoundItem from '../components/NotFoundItem';
-import IconButton from '@mui/material/IconButton';
-import AddLinkIcon from '@mui/icons-material/AddLink';
-import { useDelete } from '../hooks/useDelete';
-import { useFetch } from '../hooks/useFetch';
-import { TextField, Tooltip } from '@mui/material';
-import ConfirmModal from '../components/ConfirmModal';
-import { useDebounce } from '../hooks/useDebouce';
+    import React from 'react';
+    import Header from '../components/Header';
+    import RoundButton from '../components/RoundButton';
+    import Switch from '@mui/material/Switch';
+    import NotFoundItem from '../components/NotFoundItem';
+    import IconButton from '@mui/material/IconButton';
+    import AddLinkIcon from '@mui/icons-material/AddLink';
+    import { useDelete } from '../hooks/useDelete';
+    import { useFetch } from '../hooks/useFetch';
+    import { TextField, Tooltip } from '@mui/material';
+    import ConfirmModal from '../components/ConfirmModal';
+    import { useDebounce } from '../hooks/useDebouce';
+    import Pagination from '@mui/material/Pagination';
 
-import { useSnackbar } from 'notistack';
+    import { useSnackbar } from 'notistack';
 
-import VolunteerBox from '../components/VolunteerBox';
-import CreateVolunteerModal from '../components/CreateVolunteerModal';
-import VolunteerListSkeleton from '../components/VolunteerListSkeleton';
+    import VolunteerBox from '../components/VolunteerBox';
+    import CreateVolunteerModal from '../components/CreateVolunteerModal';
+    import VolunteerListSkeleton from '../components/VolunteerListSkeleton';
 
-const Volunteer = () => {
-    const { enqueueSnackbar } = useSnackbar();
-    const { deleteRequest } = useDelete();
-    const [confirmModalOpen, setConfirmModalOpen] = React.useState(false);
-    const [selectedVolunteer, setSelectedVolunteer] = React.useState({});
-    const [volunteerName, setVolunteerName] = React.useState('');
-    const [ministryName, setMinistryName] = React.useState('');
-    const [isLinkedToAnyMinistry, setIsLinkedToAnyMinistry] = React.useState(false);
+    const Volunteer = () => {
+        const { enqueueSnackbar } = useSnackbar();
+        const { deleteRequest } = useDelete();
+        const [page, setPage] = React.useState(0);
+        const [size, setSize] = React.useState(10);
+        const [confirmModalOpen, setConfirmModalOpen] = React.useState(false);
+        const [selectedVolunteer, setSelectedVolunteer] = React.useState({});
+        const [volunteerName, setVolunteerName] = React.useState('');
+        const [ministryName, setMinistryName] = React.useState('');
+        const [isLinkedToAnyMinistry, setIsLinkedToAnyMinistry] = React.useState(false);
 
-    const debouncedVolunteerName = useDebounce(volunteerName, 500);
-    const debouncedMinistryName = useDebounce(ministryName, 500);
-    const debouncedIsLinkedToAnyMinistry = useDebounce(isLinkedToAnyMinistry, 120);
-    
-    const { data, error, loading, fetch } = useFetch(`/volunteers?volunteerName=${volunteerName}&ministryName=${ministryName}&isLinkedToAnyMinistry=${isLinkedToAnyMinistry}`);
-    const [open, setOpen] = React.useState(false);
+        const debouncedVolunteerName = useDebounce(volunteerName, 500);
+        const debouncedMinistryName = useDebounce(ministryName, 500);
+        const debouncedIsLinkedToAnyMinistry = useDebounce(isLinkedToAnyMinistry, 120);
 
-    React.useEffect(() => {
-        document.title = 'Voluntários';
-    }, []);
+        const { data, error, loading, fetch } = useFetch(`/volunteers?volunteerName=${volunteerName}&ministryName=${ministryName}&isLinkedToAnyMinistry=${isLinkedToAnyMinistry}&page=${page}&size=${size}`);
+        const [open, setOpen] = React.useState(false);
 
-    React.useEffect(() => {
-        fetch();
-    }, [debouncedVolunteerName, debouncedMinistryName, debouncedIsLinkedToAnyMinistry]);
+        React.useEffect(() => {
+            document.title = 'Voluntários';
+        }, []);
 
-    const handleVolunteerNameChange = (event) => {
-        setVolunteerName(event.target.value);
-    };
+        React.useEffect(() => {
+            setPage(0);
+            setSize(10);
+            fetch();
+        }, [debouncedVolunteerName, debouncedMinistryName, debouncedIsLinkedToAnyMinistry]);
 
-    const handleMinistryNameChange = (event) => {
-        setMinistryName(event.target.value);
-    };
+        React.useEffect(() => {
+            fetch();
+        }, [page, size]);
 
-    const handleSwitchChange = (event) => {
-        setIsLinkedToAnyMinistry(event.target.checked);
-    };
+        const handleVolunteerNameChange = (event) => {
+            setVolunteerName(event.target.value);
+        };
 
-    const handleClick = () => {
-        setOpen(true);
-    }
+        const handleMinistryNameChange = (event) => {
+            setMinistryName(event.target.value);
+        };
 
-    const handleClose = () => {
-        setOpen(false);
-    }
+        const handleSwitchChange = (event) => {
+            setIsLinkedToAnyMinistry(event.target.checked);
+        };
 
-    const copyAutocadastroLink = async () => {
-        navigator.clipboard.writeText(`http://localhost:5173/voluntario/entrar`);
-        enqueueSnackbar('Link de autocadastro copiado.', { variant: 'success' });
-    };
-
-    const handleDeleteClick = (volunteer) => {
-        setSelectedVolunteer(volunteer);
-        setConfirmModalOpen(true);
-    }
-
-    const handleDeleteConfirm = async (id) => {
-        try {
-            const response = await deleteRequest(`/volunteers/${id}`);
-
-            if (response.status === 204) {
-                enqueueSnackbar('Voluntário removido com sucesso', { variant: 'success' });
-                fetch();
-            }
-
-        } catch (error) {
-            enqueueSnackbar(error.response?.data?.message || "Erro geral", { variant: 'error' });
-        } finally {
-            setConfirmModalOpen(false);
+        const handleClick = () => {
+            setOpen(true);
         }
-    }
 
-    return (
-        <>
-            <Header />
-            <div className="flex justify-between items-center mt-16 mx-12">
-                <div className="flex flex-1 gap-4">
-                    <TextField
-                        label="Voluntários"
-                        variant="standard"
-                        size="small"
-                        value={volunteerName}
-                        onChange={handleVolunteerNameChange}
-                        sx={{ width: '300px' }}
-                    />
+        const handleClose = () => {
+            setOpen(false);
+        }
 
-                    <TextField
-                        label="Ministério"
-                        variant="standard"
-                        size="small"
-                        value={ministryName}
-                        onChange={handleMinistryNameChange}
-                        sx={{ width: '300px' }}
-                    />
-                </div>
-                <div>
-                    <RoundButton value="CADASTRAR VOLUNTÁRIO" onClick={handleClick} />
-                    <Tooltip title="Copiar link de autocadastro" arrow>
-                        <IconButton onClick={copyAutocadastroLink} sx={{ ml: '5px' }}>
-                            <AddLinkIcon />
-                        </IconButton>
-                    </Tooltip>
-                </div>
-            </div>
-            <div className="ml-10 mt-4">
-                <Switch
-                    checked={isLinkedToAnyMinistry}
-                    onChange={handleSwitchChange}
-                    name="checked"
-                    sx={{
-                        '& .MuiSwitch-switchBase.Mui-checked': {
-                            color: "#4169E1",
-                            '& + .MuiSwitch-track': {
-                                backgroundColor: "#4169E1",
-                            },
-                        },
-                    }}
-                />
-                <span className="text-quinary">Apenas voluntários com ministérios</span>
-            </div>
-            {!loading && data?.volunteers?.length === 0 ? (
-                <NotFoundItem entities="voluntários" />
-            ) : (
-                <div className="bg-septenary p-4 mx-12 mt-12">
-                    {data?.volunteers.map((volunteer) => (
-                        <VolunteerBox
-                            key={volunteer.id}
-                            volunteer={volunteer}
-                            ministries={volunteer.ministries}
-                            handleDeleteClick={() => handleDeleteClick(volunteer)}
+        const copyAutocadastroLink = async () => {
+            navigator.clipboard.writeText(`http://localhost:5173/voluntario/entrar`);
+            enqueueSnackbar('Link de autocadastro copiado.', { variant: 'success' });
+        };
+
+        const handleDeleteClick = (volunteer) => {
+            setSelectedVolunteer(volunteer);
+            setConfirmModalOpen(true);
+        }
+
+        const handleDeleteConfirm = async (id) => {
+            try {
+                const response = await deleteRequest(`/volunteers/${id}`);
+
+                if (response.status === 204) {
+                    enqueueSnackbar('Voluntário removido com sucesso', { variant: 'success' });
+                    fetch();
+                }
+
+            } catch (error) {
+                enqueueSnackbar(error.response?.data?.message || "Erro geral", { variant: 'error' });
+            } finally {
+                setConfirmModalOpen(false);
+            }
+        }
+
+        return (
+            <>
+                <Header />
+                <div className="flex justify-between items-center mt-16 mx-12">
+                    <div className="flex flex-1 gap-4">
+                        <TextField
+                            label="Voluntários"
+                            variant="standard"
+                            size="small"
+                            value={volunteerName}
+                            onChange={handleVolunteerNameChange}
+                            sx={{ width: '300px' }}
                         />
-                    ))}
-                </div>
-            )}
-            <CreateVolunteerModal
-                open={open}
-                setOpen={setOpen}
-                handleClose={handleClose}
-                fetch={fetch}
-            />
-            <ConfirmModal
-                open={confirmModalOpen}
-                onClose={() => setConfirmModalOpen(false)}
-                title="Remover voluntário"
-                content="Deseja realmente remover este voluntário? Esta ação não poderá ser desfeita."
-                action={() => handleDeleteConfirm(selectedVolunteer?.id)}
-            />
-        </>
-    );
-};
 
-export default Volunteer;
+                        <TextField
+                            label="Ministério"
+                            variant="standard"
+                            size="small"
+                            value={ministryName}
+                            onChange={handleMinistryNameChange}
+                            sx={{ width: '300px' }}
+                        />
+                    </div>
+                    <div>
+                        <RoundButton value="CADASTRAR VOLUNTÁRIO" onClick={handleClick} />
+                        <Tooltip title="Copiar link de autocadastro" arrow>
+                            <IconButton onClick={copyAutocadastroLink} sx={{ ml: '5px' }}>
+                                <AddLinkIcon />
+                            </IconButton>
+                        </Tooltip>
+                    </div>
+                </div>
+                <div className="ml-10 mt-4">
+                    <Switch
+                        checked={isLinkedToAnyMinistry}
+                        onChange={handleSwitchChange}
+                        name="checked"
+                        sx={{
+                            '& .MuiSwitch-switchBase.Mui-checked': {
+                                color: "#4169E1",
+                                '& + .MuiSwitch-track': {
+                                    backgroundColor: "#4169E1",
+                                },
+                            },
+                        }}
+                    />
+                    <span className="text-quinary">Apenas voluntários com ministérios</span>
+                </div>
+                {loading && <VolunteerListSkeleton />}
+                {!loading && data?.volunteers?.length === 0 ? (
+                    <NotFoundItem entities="voluntários" />
+                ) : (
+                    <>
+                        <div className="bg-septenary p-4 mx-12 mt-12">
+                            {data?.volunteers.map((volunteer) => (
+                                <VolunteerBox
+                                    key={volunteer.id}
+                                    volunteer={volunteer}
+                                    ministries={volunteer.ministries}
+                                    handleDeleteClick={() => handleDeleteClick(volunteer)}
+                                />
+                            ))}
+                        </div>
+                        <Pagination
+                            count={Math.ceil(data?.totalElements / size)}
+                            page={page + 1}
+                            onChange={(event, newPage) => setPage(newPage - 1)}
+                            color="primary"
+                            sx={{ display: 'flex', justifyContent: 'right', mt: 2, mb: 2, mr: 4 }}
+                        />
+                    </>
+                )}
+                <CreateVolunteerModal
+                    open={open}
+                    setOpen={setOpen}
+                    handleClose={handleClose}
+                    fetch={fetch}
+                />
+                <ConfirmModal
+                    open={confirmModalOpen}
+                    onClose={() => setConfirmModalOpen(false)}
+                    title="Remover voluntário"
+                    content="Deseja realmente remover este voluntário? Esta ação não poderá ser desfeita."
+                    action={() => handleDeleteConfirm(selectedVolunteer?.id)}
+                />
+            </>
+        );
+    };
+
+    export default Volunteer;
