a56cad45506b06b556cfe2c832587e98305de0ef
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-02-24 22:25:04 -0300

    impr: ministry line

--- src/components/MinistryLine.jsx
+++ src/components/MinistryLine.jsx
@@ -1,30 +1,32 @@
 import React from 'react';
 import { Edit, Delete } from '@mui/icons-material';
-import { IconButton, Tooltip } from '@mui/material';
+import { IconButton, Tooltip, TableRow, TableCell } from '@mui/material';
 
 import MinistryBox from './MinistryBox';
 
 const MinistryLine = ({ ministry, onMinistryNameClick, handleEdit }) => {
     return (
-        <tr className="border-b">
-            <td className="text-center p-2">
+        <TableRow>
+            <TableCell align="center">
                 <MinistryBox name={ministry.name} color={ministry.color} onClick={onMinistryNameClick} />
-            </td>
-            <td className="text-left p-10">{ministry.description}</td>
-            <td className="text-center font-bold text-quinary p-10 hover:underline cursor-pointer" onClick={onMinistryNameClick}>{ministry.totalVolunteers}</td>
-            <td className="text-right p-4">
+            </TableCell>
+            <TableCell sx={{ fontSize: '1rem' }}>{ministry.description}</TableCell>
+            <TableCell align="center" sx={{ fontWeight: 'bold', color: '#454545', '&:hover': { textDecoration: 'underline', cursor: 'pointer' } }} onClick={onMinistryNameClick}>
+                {ministry.totalVolunteers}
+            </TableCell>
+            <TableCell align="right">
                 <Tooltip title="Editar">
                     <IconButton onClick={handleEdit}>
-                        <Edit className="cursor-pointer mx-2" />
+                        <Edit />
                     </IconButton>
                 </Tooltip>
                 <Tooltip title="Excluir">
                     <IconButton>
-                        <Delete className="cursor-pointer" />
+                        <Delete />
                     </IconButton>
                 </Tooltip>
-            </td>
-        </tr>
+            </TableCell>
+        </TableRow>
     );
 };
 
