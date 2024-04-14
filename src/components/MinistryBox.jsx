02580a8da56f9c0f9bc4de33af9927a5c8dda2d1
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-04-14 19:05:37 -0300

    ministry box ajustado

--- src/components/MinistryBox.jsx
+++ src/components/MinistryBox.jsx
@@ -1,14 +1,28 @@
 import React from 'react';
+import { Box, Typography } from '@mui/material';
 
 const MinistryBox = ({ name, color, onClick }) => {
     return (
-        <div
-            className="flex justify-center items-center rounded-lg hover:underline cursor-pointer text-white"
-            style={{ width: 270, height: 55, backgroundColor: color }}
+        <Box
+            sx={{
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                width: 270,
+                height: 55,
+                backgroundColor: color,
+                borderRadius: '12px',
+                cursor: 'pointer',
+                '&:hover': {
+                    boxShadow: '0 0 10px rgba(0, 0, 0, 0.55)',
+                }
+            }}
             onClick={onClick}
         >
-            <span className="text-white">{name}</span>
-        </div>
+            <Typography variant="body1" color="white">
+                {name}
+            </Typography>
+        </Box>
     );
 };
 
--- src/components/OpenScheduleModal.jsx
+++ src/components/OpenScheduleModal.jsx
@@ -25,7 +25,7 @@ const style = {
     bgcolor: 'background.paper',
     boxShadow: 24,
     p: 4,
-    borderRadius: 1,
+    borderRadius: 2,
 };
 
 const textFieldStyle = {
