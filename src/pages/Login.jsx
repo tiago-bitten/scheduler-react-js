9c68facfbd4eb662ef4d75c5fe4d610278d66f8f
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-04-17 10:38:09 -0300

    alterar tela login

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,6 +1,19 @@
 import React from 'react';
 import LoginRightCard from '../components/LoginRightCard';
 import LoginLeftCard from '../components/LoginLeftCard';
+import { Box } from '@mui/material';
+
+/**
+ * 
+ * colors: {
+        'primary': "#4169E1",
+        'secondary': "#718FE9",
+        'tertiary': "#A0B4F0",
+        'quaternary': "#ffffff",
+        'quinary': "#454545",
+        'senary': "#D9D9D9",
+        'septenary': "#F3F3F3",
+ */
 
 const Login = () => {
     React.useEffect(() => {
@@ -8,14 +21,40 @@ const Login = () => {
     }, []);
 
     return (
-        <div className="bg-primary w-full h-screen flex justify-center items-center shadow">
-            <div className="bg-tertiary flex items-center justify-center" style={{ width: '450px', height: '550px' }}>
+        <Box sx={{
+            width: '100%',
+            height: '100vh',
+            display: 'flex',
+            justifyContent: 'center',
+            alignItems: 'center',
+            bgcolor: '#4169E1',
+            boxShadow: 1,
+        }}>
+            <Box sx={{
+                bgcolor: '#A0B4F0', 
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: 450,
+                height: 550,
+                borderTopLeftRadius: 8,
+                borderBottomLeftRadius: 8
+            }}>
                 <LoginLeftCard />
-            </div>
-            <div className="bg-white flex items-center justify-center" style={{ width: '450px', height: '550px' }}>
+            </Box>
+            <Box sx={{
+                bgcolor: 'background.paper',
+                display: 'flex',
+                alignItems: 'center',
+                justifyContent: 'center',
+                width: 450,
+                height: 550,
+                borderTopRightRadius: 8,
+                borderBottomRightRadius: 8
+            }}>
                 <LoginRightCard />
-            </div>
-        </div>
+            </Box>
+        </Box>
     );
 };
 
