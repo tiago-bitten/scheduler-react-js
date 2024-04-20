4126123997a6e6ad1fa12128be4a92cacf770c34
BuildTools <tiago.sbittencourt@icloud.com>
2024-04-19 21:13:16 -0300

    volunteer associate ministry

---
+++ src/components/VolunteerAssociateMinistryModal.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Box, Modal, Typography } from '@mui/material';
+
+const boxStyle = {
+    
+}
+
+const VolunteerAssociateMinistryModal = ({ open, onClose, volunteer, fetchVolunteers }) => {
+};
+
+export default VolunteerAssociateMinistryModal;
