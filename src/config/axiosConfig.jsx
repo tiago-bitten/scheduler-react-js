f452101be80856582e45c6142b1a6c2d60063ea8
tiago-bitten <tiago.sbittencourt@icloud.com>
2024-08-19 11:32:52 -0300

    meu deus // naoaoaoaoaoa

--- src/config/axiosConfig.jsx
+++ src/config/axiosConfig.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'https://scheduler-java-api-production.up.railway.app//api/v1'
+    baseURL: 'https://scheduler-java-api-production.up.railway.app/api/v1'
 });
 
 export default instance;
